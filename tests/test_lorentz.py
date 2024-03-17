772ab540f020c3aee202256c68d95a318240dc4a
Kai Habermann <kai.habermann@gmx.net>
2024-03-17 21:27:27 +0100

    ...

--- tests/test_lorentz.py
+++ tests/test_lorentz.py
@@ -6,21 +6,12 @@ import numpy as np
 
 jax.config.update("jax_enable_x64", True)
 def test_lotentz():
-    m = 139.57018 # MeV
-    p = jnp.array([-400., 0, 0])
-    P = jnp.array([*p, (m**2 + jnp.sum(p**2))**0.5])
-
-  
     psi, theta, xi, theta_rf, phi_rf, psi_rf = -1.8, 1.3, 1.4, 1.5, 2.6, -2.1 + 2* np.pi
     M = build_4_4(psi, theta, xi, theta_rf, phi_rf, psi_rf)
-
-
-    M2 = build_2_2(psi, theta, xi, theta_rf, phi_rf, psi_rf)
     trafo = LorentzTrafo(psi, theta, xi, theta_rf, phi_rf, psi_rf)
     psi_, theta_, xi_, theta_rf_, phi_rf_, psi_rf_ = trafo.decode()
 
     assert np.allclose(M, build_4_4(psi_, theta_, xi_, theta_rf_, phi_rf_, psi_rf_))
-
     assert np.allclose(psi, psi_)
     assert np.allclose(theta, theta_)
     assert np.allclose(xi, xi_)
