e576248d01c7abb9c1d7576f33ce8177acb4fbb0
Kai Habermann <kai.habermann@gmx.net>
2024-04-02 17:15:23 +0200

    Run autoapi aswell

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os, sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../src/'))
 project = 'decayangle'
 copyright = '2024, Kai Habermann, Mikhail Mikhasenko'
 author = 'Kai Habermann, Mikhail Mikhasenko'
