c0fa0f704001fa5e4ee3383a4931856f147631ab
Kai Habermann <kai.habermann@gmx.net>
2024-04-02 16:31:13 +0200

    Sphinx try 1

---
+++ doc/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'decayangle'
+copyright = '2024, Kai Habermann, Mikhail Mikhasenko'
+author = 'Kai Habermann, Mikhail Mikhasenko'
+release = '02.04.2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autoapi_type = 'python'
+autoapi_dir = '../src/decayangle'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
